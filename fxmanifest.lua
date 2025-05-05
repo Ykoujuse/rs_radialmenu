2d07935ce59e57d7b3741a3496ead12a51e0cffc
Rsheng <115930547+Ykoujuse@users.noreply.github.com>
2025-05-05 18:02:26 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+author "Rsheng-QQ:3391407547"
+lua54 'yes'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/script.js'
+}
+
